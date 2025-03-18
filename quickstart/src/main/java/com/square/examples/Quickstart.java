7ac190ef6ae8f53d48b9bcdb758f942be58c97c2
Mike Konopelski <mikek@squareup.com>
2025-03-18 15:55:13 -0400

    upgrade quickstart to work with new sdk

--- quickstart/src/main/java/com/square/examples/Quickstart.java
+++ quickstart/src/main/java/com/square/examples/Quickstart.java
@@ -4,16 +4,15 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-import com.squareup.square.*;
-import com.squareup.square.api.*;
-import com.squareup.square.authentication.BearerAuthModel;
-import com.squareup.square.models.*;
-import com.squareup.square.models.Error;
-import com.squareup.square.exceptions.*;
+import com.squareup.square.AsyncSquareClient;
+import com.squareup.square.core.Environment;
+import com.squareup.square.types.Location;
+import com.squareup.square.types.Error;
+import com.squareup.square.core.SquareApiException;
+import com.squareup.square.types.Address;
 
 public class Quickstart {
     public static void main(String[] args) {
-
         InputStream inputStream =
             Quickstart.class.getResourceAsStream("/config.properties");
         Properties prop = new Properties();
@@ -25,37 +24,59 @@ public class Quickstart {
             e.printStackTrace();
         }
 
-        SquareClient client = new SquareClient.Builder()
-        .bearerAuthCredentials(new BearerAuthModel.Builder(prop.getProperty("SQUARE_ACCESS_TOKEN")).build())
-        .environment(Environment.SANDBOX)
-        .build();
+        AsyncSquareClient client = AsyncSquareClient.builder()
+            .token(prop.getProperty("SQUARE_ACCESS_TOKEN"))
+            .environment(Environment.SANDBOX)
+            .build();
 
-        LocationsApi locationsApi = client.getLocationsApi();
+        client.locations().list()
+            .thenAccept(result -> {
+                System.out.println("Location(s) for this account:");
+                result.getLocations().ifPresent(locations -> {
+                    for (Location l : locations) {
+                        // Build location details safely
+                        StringBuilder locationInfo = new StringBuilder();
+                        
+                        // Add ID if present
+                        l.getId().ifPresent(id -> locationInfo.append("ID: ").append(id));
+                        
+                        // Add name if present
+                        l.getName().ifPresent(name -> 
+                            locationInfo.append(locationInfo.length() > 0 ? ", " : "")
+                                     .append("Name: ").append(name));
+                        
+                        // Handle address details if present
+                        l.getAddress().ifPresent(address -> {
+                            // Add address line 1 if present
+                            address.getAddressLine1().ifPresent(line1 -> 
+                                locationInfo.append(locationInfo.length() > 0 ? ", " : "")
+                                         .append("Address: ").append(line1));
+                            
+                            // Add locality (city) if present
+                            address.getLocality().ifPresent(locality -> 
+                                locationInfo.append(locationInfo.length() > 0 ? ", " : "")
+                                         .append("City: ").append(locality));
+                        });
 
-        locationsApi.listLocationsAsync().thenAccept(result -> {
-            System.out.println("Location(s) for this account:");
-            for (Location l : result.getLocations()) {
-                System.out.printf("%s: %s, %s, %s\n",
-                    l.getId(), l.getName(),
-                    l.getAddress().getAddressLine1(),
-                    l.getAddress().getLocality());
-            }
-
-        }).exceptionally(exception -> {
-            try {
-                throw exception.getCause();
-            } catch (ApiException ae) {
-                for (Error err : ae.getErrors()) {
-                    System.out.println(err.getCategory());
-                    System.out.println(err.getCode());
-                    System.out.println(err.getDetail());
+                        // Print the complete location information
+                        System.out.println(locationInfo.toString());
+                    }
+                });
+            })
+            .exceptionally(exception -> {
+                if (exception.getCause() instanceof SquareApiException) {
+                    SquareApiException apiException = (SquareApiException) exception.getCause();
+                    apiException.errors().forEach(error -> {
+                        System.out.println("Category: " + error.getCategory());
+                        System.out.println("Code: " + error.getCode());
+                        System.out.println("Detail: " + error.getDetail());
+                    });
+                } else {
+                    System.out.println("An unexpected error occurred:");
+                    exception.printStackTrace();
                 }
-            } catch (Throwable t) {
-                t.printStackTrace();
-            }
-            return null;
-        }).join();
-
-        SquareClient.shutdown();
+                return null;
+            })
+            .join();
     }
 }
